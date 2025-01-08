df91f5aa2b6f007a301cf61c11154ab7032ac202
Vadim Kiniabaev <thekinv21@gmail.com>
2025-01-08 09:39:11 +0300

    feat: add components.json for component configuration, update package.json and pnpm-lock.yaml with new dependencies, enhance tailwind.config.ts for custom theming, and improve index.scss with CSS variables

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,38 @@
 import { Button } from './button'
 
-export { Button }
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger
+} from './dropdown-menu'
+
+export {
+	Button,
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuPortal,
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger
+}
